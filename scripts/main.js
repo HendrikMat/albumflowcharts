d5319192216b7aa61ac3c2c046d64c35e0a1908c
HendrikMatvejev <hendrik.matvejev2@gmail.com>
2018-03-04 18:07:54 +0200

    Finalized the flowchart. Page scrolls with buttons. Buttons disable.

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,70 @@ function unhide(divID) {
 
   }
 }
+$('.firstgenbtn').click(() => {
+  $('html, body').animate({
+    scrollTop: $('.firstgenbtn').offset().top
+  }, 1000);
+});
+
+$('.secondgenbtn').click(() => {
+  $('html, body').animate({
+    scrollTop: $('#btntxt2').offset().top
+  }, 1000);
+});
+
+$('.thirdgenbtn').click(() => {
+  $('html, body').animate({
+    scrollTop: $('#btntxt3').offset().top
+  }, 1000);
+});
+
+$('.fourthgenbtn').click(() => {
+  $('html, body').animate({
+    scrollTop: $('#btntxt4').offset().top
+  }, 1000);
+});
+
+function unhidebtn1(divID) {
+
+  var item = document.getElementById(divID);
+
+  if (item) {
+
+    item.className = (item.className == "hidden") ? "unhidden" : "hidden";
+    $(".firstgenbtn").prop('disabled', true);
+  }
+}
+
+function unhidebtn2(divID) {
+
+  var item = document.getElementById(divID);
+
+  if (item) {
+
+    item.className = (item.className == "hidden") ? "unhidden" : "hidden";
+    $(".secondgenbtn").prop('disabled', true);
+  }
+}
+
+function unhidebtn3(divID) {
+
+  var item = document.getElementById(divID);
+
+  if (item) {
+
+    item.className = (item.className == "hidden") ? "unhidden" : "hidden";
+    $(".thirdgenbtn").prop('disabled', true);
+  }
+}
+
+function unhidebtn4(divID) {
+
+  var item = document.getElementById(divID);
+
+  if (item) {
+
+    item.className = (item.className == "hidden") ? "unhidden" : "hidden";
+    $(".thirdgenbtn").prop('disabled', true);
+  }
+}
